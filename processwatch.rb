60b7f01556060a586d7bdea55acb19096bc3899f
Phil Chen <nethacker@gmail.com>
2013-04-05 14:07:35 -0400

    Changed Directory Modules to Tasks and References to it

--- processwatch.rb
+++ processwatch.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
 	dir = File.dirname(__FILE__)
-	Dir[File.expand_path("#{dir}/modules/*.rb")]. uniq. each do |file| 
+	Dir[File.expand_path("#{dir}/tasks/*.rb")]. uniq. each do |file| 
 	require file
 end
