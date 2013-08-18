53d48d8067683249e6ca4b5e7610cd5594c483dc
nethacker <nethacker@gmail.com>
2013-08-18 22:03:12 +0000

    Fixed bug in diagnostic output, heredoc order issue

--- lib/processwatch/pw_linux.rb
+++ lib/processwatch/pw_linux.rb
@@ -15,6 +15,25 @@ module Processwatch
 
     list = ps_list.split(/\n/)
 
+    occurances = list.grep(/.*#$restart_process.*/)
+    if occurances.length == 0 && $restart_mail == "yes" && $restart_start == "yes"
+      usw =  Usagewatch
+diagnostic_msgstr = <<END_OF_MESSAGE
+#{usw.uw_diskused} Total Gigabytes Disk Used
+#{usw.uw_diskused_perc} Percentage of Gigabytes Used
+#{usw.uw_cpuused}% CPU Used
+#{usw.uw_tcpused} TCP Connections Used
+#{usw.uw_udpused} UDP Connections Used
+#{usw.uw_memused}% Active Memory Used
+#{usw.uw_load} Average System Load Of The Past Minute
+#{usw.uw_bandrx} Mbit/s Current Bandwidth Received
+#{usw.uw_bandtx} Mbit/s Current Bandwidth Transmitted
+#{usw.uw_diskioreads}/s Current Disk Reads Completed
+#{usw.uw_diskiowrites}/s Current Disk Writes Completed
+Top Ten Processes By CPU Consumption: #{usw.uw_cputop}
+Top Ten Processes By Memory Consumption: #{usw.uw_memtop}
+END_OF_MESSAGE
+
 restart_msgstr = <<END_OF_MESSAGE
 From: #$restart_from <#$restart_from_email>
 To: #$restart_to <#$restart_to_email>
@@ -24,42 +43,12 @@ Process Watch has detected #{$restart_process} has died.
 
 We have started #{$restart_process} again.
 
-#{diagnostic_msgstr}
-
--Process Watch
-
-END_OF_MESSAGE
-
-dead_process_msgstr = <<END_OF_MESSAGE
-From: #$restart_from <#$restart_from_email>
-To: #$restart_to <#$restart_to_email>
-Subject: Dead Process Detected On #{host}
-
-Process Watch has detected #{$restart_process} has died.
+Below Is a Snapshot Of System Statistics Before The Process Was Started Again
 
 #{diagnostic_msgstr}
 
 -Process Watch
 
-END_OF_MESSAGE
-
-    occurances = list.grep(/.*#$restart_process.*/)
-    if occurances.length == 0 && $restart_mail == "yes" && $restart_start == "yes"
-      usw =  Usagewatch
-diagnostic_msgstr = <<END_OF_MESSAGE
-- #{usw.uw_diskused} Gigabytes Used
-- #{usw.uw_diskused_perc} Perventage of Gigabytes Used
-- #{usw.uw_cpuused}% CPU Used
-- #{usw.uw_tcpused} TCP Connections Used
-- #{usw.uw_udpused} UDP Connections Used
-- #{usw.uw_memused}% Active Memory Used
-- #{usw.uw_load} Average System Load Of The Past Minute
-- #{usw.uw_bandrx} Mbit/s Current Bandwidth Received
-- #{usw.uw_bandtx} Mbit/s Current Bandwidth Transmitted
-- #{usw.uw_diskioreads}/s Current Disk Reads Completed
-- #{usw.uw_diskiowrites}/s Current Disk Writes Completed
-- Top Ten Processes By CPU Consumption: #{usw.uw_cputop}
-- Top Ten Processes By Memory Consumption: #{usw.uw_memtop}
 END_OF_MESSAGE
         system $restart_action
           require 'net/smtp'
@@ -69,19 +58,34 @@ END_OF_MESSAGE
     elsif occurances.length == 0 && $restart_mail == "yes" && $restart_start == "no"
       usw =  Usagewatch
 diagnostic_msgstr = <<END_OF_MESSAGE
-- #{usw.uw_diskused} Gigabytes Used
-- #{usw.uw_diskused_perc} Perventage of Gigabytes Used
-- #{usw.uw_cpuused}% CPU Used
-- #{usw.uw_tcpused} TCP Connections Used
-- #{usw.uw_udpused} UDP Connections Used
-- #{usw.uw_memused}% Active Memory Used
-- #{usw.uw_load} Average System Load Of The Past Minute
-- #{usw.uw_bandrx} Mbit/s Current Bandwidth Received
-- #{usw.uw_bandtx} Mbit/s Current Bandwidth Transmitted
-- #{usw.uw_diskioreads}/s Current Disk Reads Completed
-- #{usw.uw_diskiowrites}/s Current Disk Writes Completed
-- Top Ten Processes By CPU Consumption: #{usw.uw_cputop}
-- Top Ten Processes By Memory Consumption: #{usw.uw_memtop}
+#{usw.uw_diskused} Total Gigabytes Disk Used
+#{usw.uw_diskused_perc} Percentage of Gigabytes Used
+#{usw.uw_cpuused}% CPU Used
+#{usw.uw_tcpused} TCP Connections Used
+#{usw.uw_udpused} UDP Connections Used
+#{usw.uw_memused}% Active Memory Used
+#{usw.uw_load} Average System Load Of The Past Minute
+#{usw.uw_bandrx} Mbit/s Current Bandwidth Received
+#{usw.uw_bandtx} Mbit/s Current Bandwidth Transmitted
+#{usw.uw_diskioreads}/s Current Disk Reads Completed
+#{usw.uw_diskiowrites}/s Current Disk Writes Completed
+Top Ten Processes By CPU Consumption: #{usw.uw_cputop}
+Top Ten Processes By Memory Consumption: #{usw.uw_memtop}
+END_OF_MESSAGE
+
+dead_process_msgstr = <<END_OF_MESSAGE
+From: #$restart_from <#$restart_from_email>
+To: #$restart_to <#$restart_to_email>
+Subject: Dead Process Detected On #{host}
+
+Process Watch has detected #{$restart_process} has died.
+
+Below Is A Snapshot Of Current System Statistics
+
+#{diagnostic_msgstr}
+
+-Process Watch
+
 END_OF_MESSAGE
       require 'net/smtp'
       Net::SMTP.start("#$restart_smtp_host", "#$restart_smtp_port") do |smtp|
